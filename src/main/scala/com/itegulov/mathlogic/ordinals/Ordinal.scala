5e5bdba55fc483fdc8bbc2b3ab79dca2bcade75e
Daniyar Itegulov <wibkwibk@gmail.com>
2015-08-07 19:35:19 +0300

    Added Ordinal-to-CNF conversion

--- src/main/scala/com/itegulov/mathlogic/ordinals/Ordinal.scala
+++ src/main/scala/com/itegulov/mathlogic/ordinals/Ordinal.scala
@@ -10,29 +10,29 @@ sealed trait Ordinal {
 case class *(left: Ordinal, right: Ordinal) extends Ordinal {
   override def toString: String = "(" + left + "*" + right + ")"
 
-  override def convertToCNF: CNF = ???
+  override def convertToCNF: CNF = left.convertToCNF * right.convertToCNF
 }
 
 case class +(left: Ordinal, right: Ordinal) extends Ordinal {
   override def toString: String = "(" + left + "+" + right + ")"
 
-  override def convertToCNF: CNF = ???
+  override def convertToCNF: CNF = left.convertToCNF + right.convertToCNF
 }
 
 case class ^(left: Ordinal, right: Ordinal) extends Ordinal {
   override def toString: String = "(" + left + "^" + right + ")"
 
-  override def convertToCNF: CNF = ???
+  override def convertToCNF: CNF = left.convertToCNF ^ right.convertToCNF
 }
 
 case class Num(number: Int) extends Ordinal {
   override def toString: String = number.toString
 
-  override def convertToCNF: CNF = ???
+  override def convertToCNF: CNF = Atom(BigInt(number))
 }
 
 case class ω() extends Ordinal {
   override def toString: String = "ω"
 
-  override def convertToCNF: CNF = ???
+  override def convertToCNF: CNF = Poly(List(ωElement(one, 1)), zero)
 }
