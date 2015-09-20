b307dff5f52162736d6977682c564c39d13d35a6
itegulov <ditegulov@gmail.com>
2015-09-20 23:27:40 +0300

    Refactored OrdinalParser

--- src/main/scala/com/itegulov/mathlogic/ordinals/OrdinalParser.scala
+++ src/main/scala/com/itegulov/mathlogic/ordinals/OrdinalParser.scala
@@ -9,28 +9,38 @@ import scala.language.implicitConversions
  * @author Daniyar Itegulov
  */
 class OrdinalParser(val input: ParserInput) extends Parser {
-  type RO = Rule1[Ordinal]
+
   implicit private def wrpStr(s: String): Rule0 = rule {
     zeroOrMore(' ') ~ str(s) ~ zeroOrMore(' ')
   }
 
-  private def leftAssoc[A](a: => Rule1[A], b: (A, A) => A, divider: String): Rule1[A]
-  = rule { a ~ zeroOrMore(wrpStr(divider) ~ a ~> b) }
+  private def leftAssoc[A](a: => Rule1[A], b: (A, A) => A, divider: String): Rule1[A] = rule {
+    a ~ zeroOrMore(wrpStr(divider) ~ a ~> b)
+  }
 
-  def equalityOrdinals: Rule1[(Ordinal, Ordinal)] =
-    rule {
-      oneOrMore(ordExpr).separatedBy("=") ~> ((sq: Seq[Ordinal]) => (sq.head, sq(1)))
+  def equalityOrdinals: Rule1[(Ordinal, Ordinal)] = rule {
+    ordExpr ~ "=" ~ ordExpr ~> {
+      (left: Ordinal, right: Ordinal) => (left, right)
     }
+  }
 
-  def ordExpr: RO  = leftAssoc(summable, ordinals.+, "+")
-  private def summable: RO = leftAssoc(mullable, ordinals.*, "*")
-  private def mullable: RO = rule {
+  def ordExpr: Rule1[Ordinal] = leftAssoc(summable, ordinals.+, "+")
+
+  private def summable: Rule1[Ordinal] = leftAssoc(mullable, ordinals.*, "*")
+
+  private def mullable: Rule1[Ordinal] = rule {
     oneOrMore(unary).separatedBy("^") ~> ((_: Seq[Ordinal]).reduceRight(ordinals.^))
   }
-  private def unary: RO = rule { omega | numeric | ("(" ~ ordExpr ~ ")") }
-  private def omega: RO = rule { ch('w') ~> (() => ω()) }
-  private def numeric: RO =
-    rule { capture(oneOrMore(anyOf("0123456789"))) ~>
-      ((s: String) => Num(Integer.parseInt(s)))
-    }
+
+  private def unary: Rule1[Ordinal] = rule {
+    omega | number | ("(" ~ ordExpr ~ ")")
+  }
+
+  private def omega: Rule1[Ordinal] = rule {
+    ch('w') ~> (() => ω())
+  }
+
+  private def number: Rule1[Ordinal] = rule {
+    capture(oneOrMore(anyOf("0123456789"))) ~> ((s: String) => Num(Integer.parseInt(s)))
+  }
 }
